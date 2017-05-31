cf0ebb2ebe393fe77fba3b4a5e53391a0b0bbb21
ybarborini <yannick.barborini@ccsd.cnrs.fr>
2017-05-31 12:22:38 +0200

    Update test_all.py

--- test_all.py
+++ test_all.py
@@ -1,11 +1,5 @@
 import numpy as np
 
-#def test_addition():
-#    assert 1 + 1 == 2
-
-#def test_division():
-#    assert 3./2 == 1.5
-
 def test_numpy_partition():
     a = np.array([3, 2, -1, 10])
     part = np.partition(a, 1)
